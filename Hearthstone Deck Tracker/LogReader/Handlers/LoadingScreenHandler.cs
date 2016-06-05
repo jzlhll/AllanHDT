5e872043386a8c39f4b89664e00b5018e363609f
allan.jiang.home <jzl.hll@163.com>
2016-06-05 22:46:04 +0800

    update master to v0.15.3

--- Hearthstone Deck Tracker/LogReader/Handlers/LoadingScreenHandler.cs
+++ Hearthstone Deck Tracker/LogReader/Handlers/LoadingScreenHandler.cs
@@ -1,11 +1,18 @@
 ﻿#region
 
 using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using HearthMirror.Enums;
 using Hearthstone_Deck_Tracker.Enums.Hearthstone;
 using Hearthstone_Deck_Tracker.Hearthstone;
 using Hearthstone_Deck_Tracker.Importing;
 using Hearthstone_Deck_Tracker.LogReader.Interfaces;
+using Hearthstone_Deck_Tracker.Utility.Extensions;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+using Hearthstone_Deck_Tracker.Windows;
 
 #endregion
 
@@ -14,6 +21,8 @@ namespace Hearthstone_Deck_Tracker.LogReader.Handlers
 	public class LoadingScreenHandler
 	{
 		private DateTime _lastAutoImport;
+		private bool _checkedMirrorStatus;
+
 		public void Handle(LogLineItem logLine, IHsGameState gameState, IGame game)
 		{
 			var match = HsLogReaderConstants.GameModeRegex.Match(logLine.Line);
@@ -32,6 +41,33 @@ namespace Hearthstone_Deck_Tracker.LogReader.Handlers
 
 			if(game.PreviousMode == Mode.GAMEPLAY)
 				gameState.GameHandler.HandleInMenu();
+
+			if(game.CurrentMode == Mode.HUB && !_checkedMirrorStatus && (DateTime.Now - logLine.Time).TotalSeconds < 5)
+				CheckMirrorStatus();
+		}
+
+		private async void CheckMirrorStatus()
+		{
+			_checkedMirrorStatus = true;
+			HearthMirror.Status status;
+			while((status = HearthMirror.Status.GetStatus()).MirrorStatus == MirrorStatus.ProcNotFound)
+				await Task.Delay(1000);
+			Log.Info($"Mirror status: {status.MirrorStatus}");
+			if(status.MirrorStatus != MirrorStatus.Error)
+				return;
+			Log.Error(status.Exception);
+			if(!(status.Exception is Win32Exception))
+			{
+				Log.Info("Not a Win32Exception - Process probably exited. Checking again later.");
+				_checkedMirrorStatus = false;
+				return;
+			}
+			LogReaderManager.Stop(true).Forget();
+			Core.MainWindow.ActivateWindow();
+			while(Core.MainWindow.Visibility != Visibility.Visible || Core.MainWindow.WindowState == WindowState.Minimized)
+				await Task.Delay(100);
+			await Core.MainWindow.ShowMessage("Uneven permissions",
+				"It appears that Hearthstone (Battle.net) and HDT do not have the same permissions.\n\nPlease run both as administrator or local user.\n\nIf you don't know what any of this means, just run HDT as administrator.");
 		}
 
 		private Mode GetMode(string modeString)
