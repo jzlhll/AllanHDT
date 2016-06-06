ee91f78e7f5e3a708341614c28f8f8d527477b95
allan.jiang.home <jzl.hll@163.com>
2016-06-06 23:59:55 +0800

    汉化热键

--- Hearthstone Deck Tracker/Utility/HotKeys/PredefinedHotKeyActions.cs
+++ Hearthstone Deck Tracker/Utility/HotKeys/PredefinedHotKeyActions.cs
@@ -50,7 +50,7 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			}
 		}
 
-		[PredefinedHotKeyAction("Toggle overlay", "Turns the overlay on or off (if the game is running).")]
+		[PredefinedHotKeyAction("切换界面", "切换界面显示和关闭(如果游戏运行中)")]
 		public static void ToggleOverlay()
 		{
 			if(!Core.Game.IsRunning)
@@ -60,8 +60,8 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.Overlay.UpdatePosition();
 		}
 
-		[PredefinedHotKeyAction("Toggle overlay: card marks",
-			"Turns the card marks and age on the overlay on or off (if the game is running).")]
+		[PredefinedHotKeyAction("切换界面:卡牌标记",
+            "在界面上切换卡牌标记和回合数显示和关闭(如果游戏运行中)")]
 		public static void ToggleOverlayCardMarks()
 		{
 			if(!Core.Game.IsRunning)
@@ -72,7 +72,7 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.Overlay.UpdatePosition();
 		}
 
-		[PredefinedHotKeyAction("Toggle overlay: secrets", "Turns the secrets panel on the overlay on or off (if the game is running).")]
+		[PredefinedHotKeyAction("切换界面:奥秘", "在界面上切换奥秘的显示和关闭(如果游戏运行中)")]
 		public static void ToggleOverlaySecrets()
 		{
 			if(!Core.Game.IsRunning)
@@ -82,7 +82,7 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.Overlay.UpdatePosition();
 		}
 
-		[PredefinedHotKeyAction("Toggle overlay: timers", "Turns the timers on the overlay on or off (if the game is running).")]
+		[PredefinedHotKeyAction("切换界面:计时器", "在界面上切换计时器的显示和关闭(如果游戏运行中)")]
 		public static void ToggleOverlayTimer()
 		{
 			if(!Core.Game.IsRunning)
@@ -92,7 +92,7 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.Overlay.UpdatePosition();
 		}
 
-		[PredefinedHotKeyAction("Toggle overlay: attack icons", "Turns both attack icons on the overlay on or off (if the game is running).")
+		[PredefinedHotKeyAction("切换界面:攻击图标", "在界面上切换攻击图标的显示和关闭(如果游戏运行中)")
 		]
 		public static void ToggleOverlayAttack()
 		{
@@ -104,7 +104,7 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.Overlay.UpdatePosition();
 		}
 
-		[PredefinedHotKeyAction("Toggle no deck mode", "Activates \"no deck mode\" (use no deck) or selects the last used deck.")]
+		[PredefinedHotKeyAction("切换【无卡组模式】", "激活【无卡组模式】或激活最后使用的卡组。")]
 		public static void ToggleNoDeckMode()
 		{
 			if(DeckList.Instance.ActiveDeck == null)
@@ -113,15 +113,15 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 				Core.MainWindow.SelectDeck(null, true);
 		}
 
-		[PredefinedHotKeyAction("Export deck",
-			"Activates \"no deck mode\" (use no deck) or selects the last used deck. This will not show any dialogs in the main window.")]
+		[PredefinedHotKeyAction("导出卡组",
+            "激活【无卡组模式】或激活最后使用的卡组。 这将不会显示任何对话框在主窗口中。")]
 		public static void ExportDeck()
 		{
 			if(DeckList.Instance.ActiveDeck != null && Core.Game.IsInMenu)
 				DeckExporter.Export(DeckList.Instance.ActiveDeckVersion).Forget();
 		}
 
-		[PredefinedHotKeyAction("Edit active deck", "Opens the edit dialog for the active deck (if any) and brings HDT to foreground.")]
+		[PredefinedHotKeyAction("编辑激活的卡组", "打开编辑激活的卡组对话框（如果有的话）和让该软件切到前台。")]
 		public static void EditDeck()
 		{
 			if(DeckList.Instance.ActiveDeck == null)
@@ -130,28 +130,28 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Import from game: arena", "Starts the webimport process with all dialogs.")]
+		[PredefinedHotKeyAction("从游戏中导入:竞技场", "")]
 		public static void ImportFromArena()
 		{
 			Core.MainWindow.StartArenaImporting().Forget();
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Import from game: constructed", "Starts the webimport process with all dialogs.")]
+		[PredefinedHotKeyAction("从游戏中导入:构筑", "")]
 		public static void ImportFromConstructed()
 		{
 			Core.MainWindow.ShowImportDialog(false);
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Import from web", "Starts the webimport process with all dialogs.")]
+		[PredefinedHotKeyAction("从网络导入", "开始将所有对话从网络导入")]
 		public static void ImportFromWeb()
 		{
 			Core.MainWindow.ImportDeck();
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Import from web: clipboard", "Starts the webimport process without the import dialog.")]
+		[PredefinedHotKeyAction("从网络导入：剪贴板", "开始网络导入进程没有任何窗口。")]
 		public static void ImportFromWebClipboard()
 		{
 			var clipboard = Clipboard.ContainsText() ? Clipboard.GetText() : "could not get text from clipboard";
@@ -159,9 +159,9 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Import from web: highlight",
-			"Starts the webimport process without any dialogs. This sends a \"ctrl-c\" command before starting the import: just highlight the url and press the hotkey."
-			)]
+		[PredefinedHotKeyAction("从网络导入：高亮",
+            "这发出了一个“ctrl+c”命令之前启动导入：只强调URL并按下热键。"
+            )]
 		public static async void ImportFromWebHighlight()
 		{
 			SendKeys.SendWait("^c");
@@ -171,9 +171,9 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Screenshot",
-			"Creates a screenshot of the game and overlay (and everthing else in front of it). Comes with an option to automatically upload to imgur."
-			)]
+		[PredefinedHotKeyAction("截图",
+            "创建一个游戏的截图和界面（和其他的一切在它的前面）。有一个选项来自动上传到Imgur。"
+            )]
 		public static async void Screenshot()
 		{
 			var handle = User32.GetHearthstoneWindow();
@@ -196,9 +196,9 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Game Screenshot",
-			"Creates a screenshot of the game only. Comes with an option to automatically upload to imgur."
-			)]
+		[PredefinedHotKeyAction("游戏截图",
+            "创建一个游戏截图。有一个选项来自动上传到Imgur。"
+            )]
 		public static async void GameScreenshot()
 		{
 			var handle = User32.GetHearthstoneWindow();
@@ -221,33 +221,33 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Note Dialog", "Brings up the note dialog for the current (running) game.")]
+		[PredefinedHotKeyAction("备注对话框", "为当前（运行）游戏带来备注对话框。")]
 		public static void NoteDialog()
 		{
 			if(Core.Game.IsRunning && !Core.Game.IsInMenu)
 				new NoteDialog(Core.Game.CurrentGameStats).Show();
 		}
 
-		[PredefinedHotKeyAction("Start Hearthstone", "Starts the Battle.net launcher and/or Hearthstone.")]
+		[PredefinedHotKeyAction("开始炉石游戏", "打开Battle.net launcher 并且打开炉石.")]
 		public static void StartHearthstone()
 		{
 			if(Core.MainWindow.BtnStartHearthstone.IsEnabled)
 				Helper.StartHearthstoneAsync().Forget();
 		}
 
-		[PredefinedHotKeyAction("Show main window", "Brings up the main window.")]
+		[PredefinedHotKeyAction("显示主窗口", "打开主窗口")]
 		public static void ShowMainWindow()
 		{
 			Core.MainWindow.ActivateWindow();
 		}
 
-		[PredefinedHotKeyAction("Show stats", "Brings up the stats window or flyout.")]
+		[PredefinedHotKeyAction("显示统计", "打开统计窗口或者弹出。")]
 		public static void ShowStats()
 		{
 			Core.MainWindow.ShowStats(false, false);
 		}
 
-		[PredefinedHotKeyAction("Reload deck", "Resets HDT to last game start.")]
+		[PredefinedHotKeyAction("重载卡组", "重置该游戏到上次开始")]
 		public static void ReloadDeck()
 		{
 			if(DeckList.Instance.ActiveDeck == null)
@@ -256,7 +256,7 @@ namespace Hearthstone_Deck_Tracker.Utility.HotKeys
 				Core.MainWindow.SelectLastUsedDeck();
 		}
 
-		[PredefinedHotKeyAction("Close HDT", "Closes HDT.")]
+		[PredefinedHotKeyAction("关闭程序", "关闭程序")]
 		public static void CloseHdt()
 		{
 			Core.MainWindow.Close();
