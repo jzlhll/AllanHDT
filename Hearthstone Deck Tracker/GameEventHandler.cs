cea00ac91055a1e0c10f488c0d1effd73bf02b64
allan.jiang.home <jzl.hll@163.com>
2016-07-23 01:18:20 +0800

    v0.15.13

--- Hearthstone Deck Tracker/GameEventHandler.cs
+++ Hearthstone Deck Tracker/GameEventHandler.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using HearthDb.Enums;
+using HearthMirror;
 using Hearthstone_Deck_Tracker.API;
 using Hearthstone_Deck_Tracker.Enums;
 using Hearthstone_Deck_Tracker.Hearthstone;
@@ -354,6 +355,16 @@ namespace Hearthstone_Deck_Tracker
 			_game.IsInMenu = false;
 			_game.Reset();
 			_game.CacheMatchInfo();
+			_game.MetaData.ServerInfo = Reflection.GetServerInfo();
+			if(!string.IsNullOrEmpty(_game.MetaData.ServerInfo?.Address))
+			{
+				var region = Helper.GetRegionByServerIp(_game.MetaData.ServerInfo.Address);
+				if(_game.CurrentRegion == Region.UNKNOWN || region == Region.CHINA)
+				{
+					_game.CurrentRegion = region;
+					Log.Info("Set current region to" + region);
+				}
+			}
 			TurnTimer.Instance.Start(_game).Forget();
 
 			var selectedDeck = DeckList.Instance.ActiveDeckVersion;
