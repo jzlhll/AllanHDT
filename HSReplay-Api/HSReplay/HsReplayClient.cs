cf1c16a0fece7947f6c967f0e13872fbb9575529
allan.jiang.home <jzl.hll@163.com>
2016-09-25 19:52:28 +0800

    update code to 0925 II

---
+++ HSReplay-Api/HSReplay/HsReplayClient.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using HSReplay.Responses;
+using HSReplay.Web;
+using Newtonsoft.Json;
+
+namespace HSReplay
+{
+	/// <summary>
+	///     API client for hsreplay.net
+	/// </summary>
+	public class HsReplayClient
+	{
+		private readonly string _apiKey;
+		private readonly bool _testData;
+		private readonly WebClient _webClient;
+		private readonly ClientConfig _config;
+
+		/// <summary>
+		/// </summary>
+		/// <param name="apiKey">hsreplay.net API key</param>
+		/// <param name="userAgent">userAgent included in webrequests</param>
+		/// <param name="testData">Set to true when not uploading actual user data.</param>
+		public HsReplayClient(string apiKey, string userAgent = "", bool testData = false, ClientConfig config = null)
+		{
+			_apiKey = apiKey;
+			_webClient = new WebClient(userAgent);
+			_testData = testData;
+			_config = config ?? new ClientConfig();
+		}
+
+		private Header GetAuthHeader(string token) => new Header("Authorization", $"Token {token}");
+
+		private Header ApiHeader => new Header("X-Api-Key", _apiKey);
+
+		/// <summary>
+		///     Creates a new upload token
+		/// </summary>
+		/// <returns>Created upload token</returns>
+		public async Task<string> CreateUploadToken()
+		{
+			var content = _testData ? JsonConvert.SerializeObject(new {test_data = true}) : null;
+			using(var response = await _webClient.PostJsonAsync(_config.TokensUrl, content, false, ApiHeader))
+			using(var responseStream = response.GetResponseStream())
+			using(var reader = new StreamReader(responseStream))
+			{
+				var token = JsonConvert.DeserializeObject<UploadToken>(reader.ReadToEnd());
+				if(string.IsNullOrEmpty(token.Key))
+					throw new Exception("Reponse contained no upload-token.");
+				return token.Key;
+			}
+		}
+
+		/// <summary>
+		///     Returns a url, which allows for the token to be claimed.
+		///     The user has to open this url in a browser for the claiming to complete.
+		/// </summary>
+		/// <param name="token">Auth token</param>
+		/// <returns>Url for account claiming.</returns>
+		public async Task<string> GetClaimAccountUrl(string token)
+		{
+			using(var response = await _webClient.PostAsync(_config.ClaimAccountUrl, string.Empty, false, ApiHeader, GetAuthHeader(token)))
+			using(var responseStream = response.GetResponseStream())
+			using(var reader = new StreamReader(responseStream))
+				return JsonConvert.DeserializeObject<AccountClaim>(reader.ReadToEnd()).Url;
+		}
+
+		/// <summary>
+		///     Returns the status of a given auth token.
+		/// </summary>
+		/// <param name="token">Auth token</param>
+		/// <returns>Status of given auth token.</returns>
+		public async Task<AccountStauts> GetAccountStatus(string token)
+		{
+			using(var response = await _webClient.GetAsync($"{_config.TokensUrl}{token}/", ApiHeader))
+			using(var responseStream = response.GetResponseStream())
+			using(var reader = new StreamReader(responseStream))
+				return JsonConvert.DeserializeObject<AccountStauts>(reader.ReadToEnd());
+		}
+
+		/// <summary>
+		///     Creates a new log upload request. This request is then passed to UploadLog().
+		///     The request contains a ShortId, which will be the game URL on the website.
+		///     Use LogValidation.LogValidator.Validate to ensure the log is valid before making this request.
+		/// </summary>
+		/// <param name="metaData">Meta data about the match.</param>
+		/// <param name="token">Auth token</param>
+		/// <returns>Upload request, containing the future game URL</returns>
+		public async Task<LogUploadRequest> CreateUploadRequest(UploadMetaData metaData, string token)
+		{
+			var content = JsonConvert.SerializeObject(metaData);
+			using(var response = await _webClient.PostAsync(_config.UploadRequestUrl, content, true, ApiHeader, GetAuthHeader(token)))
+			using(var responseStream = response.GetResponseStream())
+			using(var reader = new StreamReader(responseStream))
+			{
+				var reponse = reader.ReadToEnd();
+				return JsonConvert.DeserializeObject<LogUploadRequest>(reponse);
+			}
+		}
+
+		/// <summary>
+		///     Uploads the given log.
+		///     The URL to of the replay is included in the LogUploadRequest object.
+		/// </summary>
+		/// <param name="request">Created by CreateUploadRequest()</param>
+		/// <param name="log">Log to be uploaded</param>
+		/// <returns></returns>
+		public async Task UploadLog(LogUploadRequest request, IEnumerable<string> log) => await UploadLog(request.PutUrl, log);
+
+		/// <summary>
+		///     Uploads the given log.
+		///     The URL to of the replay is included in the LogUploadRequest object.
+		/// </summary>
+		/// <param name="putUrl">Found in LogUploadRequest.PutUrl</param>
+		/// <param name="log">Log to be uploaded</param>
+		/// <returns></returns>
+		public async Task UploadLog(string putUrl, IEnumerable<string> log)
+			=> (await _webClient.PutAsync(putUrl, string.Join(Environment.NewLine, log), true)).Close();
+	}
+}
